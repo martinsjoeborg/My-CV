c0a380bf7348c20c35f66b94a9183023778e87b3
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2023-02-16 12:45:01 +0100

    First uppdate

---
+++ script.js
@@ -0,0 +1,15 @@
+// const content = document.getElementById('content');
+
+// console.log("test");
+
+// const container = document.createElement('main');
+// content.appendChild(container);
+// container.id = "container";
+
+// const leftSide = document.createElement('section');
+// container.appendChild(leftSide);
+// leftSide.id = "leftSide";
+
+// const rightSide = document.createElement('section');
+// container.appendChild(rightSide);
+// rightSide.id = "rightSide";
